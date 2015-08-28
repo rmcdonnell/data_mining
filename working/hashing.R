477db473f1080d48d8b71f8f083b7101f4d07bcb
rmcdonnell <rmcdonnell1990@icloud.com>
2015-08-28 13:58:07 -0400

    playing with the column names in the output matrix. Incomplete.

--- working/hashing.R
+++ working/hashing.R
@@ -1,25 +1,33 @@
 ## Hash functions associated with Ch. 3 (finding similar items)
 
-
-s1 <- unique(c("a","b","c","d","e"))
-s2 <- unique(c("a","a","b","b","e"))
-s3 <- unique(c("c","c","d","d","e"))
-s4 <- unique(c("d","d","e","e","b"))
-
-
 # matrix representation of sets
 setsToMatrix <- function(setList){
+  print(names(setList))
   main_set <- unique(unlist(setList))
-  out <- matrix(data = main_set)
-  for (item in setList){
+  out <- matrix(nrow = length(main_set))
+  column_list <- c()
+  for (n,item in setList){
+    # print(names(item))
+    column_list <- c(column_list, item)
     column <- as.numeric(main_set %in% item)
     out <- cbind(out,column)
   }
-  return(out)
+  f <- function(x) return(deparse(substitute(x)))
+  # print(lapply(setList, f))
+  # colnames(out) <- column_list
+  rownames(out) <- main_set
+  return(out[,-1])
 }
 
-setsToMatrix(list(s1,s2,s3,s4))
-
+S1 <- unique(c("a","d"))
+S2 <- unique(c("c"))
+S3 <- unique(c("b","d","e"))
+S4 <- unique(c("a","c","d"))
+testie <- setsToMatrix(list(S1,S2,S3,S4))
+testie
+summary(testie)
+lsd <- list(S1,S2,S3,S4)
+deparse(eval(substitute(lsd)))
 minHash <- function(){
   # 
 }
