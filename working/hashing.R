9e3503bb9a10d518e32fc38c660cd26c4328bd25
rmcdonnell <rmcdonnell1990@icloud.com>
2015-08-29 16:41:38 -0400

    altered the setsToMatrix to give proper names of sets, also changed the inputs to take only list of set as a list of strings where each item is the name of a set

--- working/hashing.R
+++ working/hashing.R
@@ -1,33 +1,26 @@
 ## Hash functions associated with Ch. 3 (finding similar items)
 
 # matrix representation of sets
-setsToMatrix <- function(setList){
-  print(names(setList))
-  main_set <- unique(unlist(setList))
-  out <- matrix(nrow = length(main_set))
-  column_list <- c()
-  for (n,item in setList){
-    # print(names(item))
-    column_list <- c(column_list, item)
-    column <- as.numeric(main_set %in% item)
+setsToMatrix <- function(universal_set, sets){
+  out <- matrix(nrow = length(universal_set), ncol = 0)
+  for (item in sets){
+    column <- as.numeric(universal_set %in% get(item))
     out <- cbind(out,column)
   }
-  f <- function(x) return(deparse(substitute(x)))
-  # print(lapply(setList, f))
-  # colnames(out) <- column_list
-  rownames(out) <- main_set
-  return(out[,-1])
+  colnames(out) <- sets
+  rownames(out) <- universal_set
+  return(out)
 }
 
 S1 <- unique(c("a","d"))
 S2 <- unique(c("c"))
 S3 <- unique(c("b","d","e"))
 S4 <- unique(c("a","c","d"))
-testie <- setsToMatrix(list(S1,S2,S3,S4))
-testie
-summary(testie)
-lsd <- list(S1,S2,S3,S4)
-deparse(eval(substitute(lsd)))
+set_O_sets <- c('S1','S2','S3','S4')
+univ_set <- c("a","b",'c','d','e')
+setsToMatrix(univ_set, set_O_sets)
+univ_set2 <- c('b')
+
 minHash <- function(){
-  # 
+  
 }
