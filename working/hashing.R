79ef058ae77fed6e084bb25d01791cef4f46eea8
rmcdonnell <rmcdonnell1990@icloud.com>
2015-08-29 17:16:43 -0400

    completed minHash function with tests

--- working/hashing.R
+++ working/hashing.R
@@ -12,6 +12,16 @@ setsToMatrix <- function(universal_set, sets){
   return(out)
 }
 
+minHash <- function(set_name, set_matrix){
+  set <- as.matrix(set_matrix[,set_name])
+  while(nrow(set) > 0){
+    if(set[1,] == 1){
+      return(rownames(set)[1])
+    }
+    set <- as.matrix(set[-1,])
+  }
+}
+
 S1 <- unique(c("a","d"))
 S2 <- unique(c("c"))
 S3 <- unique(c("b","d","e"))
@@ -19,8 +29,18 @@ S4 <- unique(c("a","c","d"))
 set_O_sets <- c('S1','S2','S3','S4')
 univ_set <- c("a","b",'c','d','e')
 setsToMatrix(univ_set, set_O_sets)
-univ_set2 <- c('b')
+# answer: 
+# S1 S2 S3 S4
+# a  1  0  0  1
+# b  0  0  1  0
+# c  0  1  0  1
+# d  1  0  1  1
+# e  0  0  1  0
 
-minHash <- function(){
-  
-}
+univ_set <- c('b','e','a','d','c')
+minHash('S2',setsToMatrix(univ_set, set_O_sets))
+# answer: "c"
+minHash('S3',setsToMatrix(univ_set, set_O_sets))
+# answer: "b"
+minHash('S4',setsToMatrix(univ_set, set_O_sets))
+# answer: "a"
